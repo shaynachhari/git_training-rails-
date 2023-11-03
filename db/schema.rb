0518b10f9ee4a3de1c0184515a1b10d9a69b6ab2
shayna chhari <shaynachhari@gmail.com>
2023-11-03 17:27:03 +0530

    save arezzo files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_28_222532) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_03_102015) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -44,12 +44,30 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_222532) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "address"
+    t.string "shop_no"
+  end
+
+  create_table "shops_students", id: false, force: :cascade do |t|
+    t.integer "shop_id", null: false
+    t.integer "student_id", null: false
+  end
+
   create_table "students", force: :cascade do |t|
     t.integer "roll"
     t.string "name"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.integer "contact"
+    t.string "class"
+    t.integer "article_id", null: false
+    t.index ["article_id"], name: "index_students_on_article_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -59,4 +77,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_222532) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "f_name"
+  end
+
+  add_foreign_key "students", "articles"
 end
